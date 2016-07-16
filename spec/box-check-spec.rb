5f11e7cccdb96a7f09ddb7c16dea53899cde1800
Chester L. Hoang & Jason <pair+chesterhub.codejlo@devbootcamp.com>
2016-07-15 17:03:55 -0700

    complete box.ok and add test board for rspec tests

--- spec/box-check-spec.rb
+++ spec/box-check-spec.rb
@@ -1,14 +1,24 @@
 require_relative '../sudoku'
 
 describe "box_ok?" do
-  let(:test_board) {####BOARD###}
+  let(:test_board) {[
+   [0, 8, 0, 0, 2, 0, 0, 0, 0],
+   [0, 4, 0, 5, 0, 0, 3, 2, 0],
+   [0, 2, 0, 3, 0, 9, 0, 4, 6],
+   [6, 0, 0, 0, 9, 0, 0, 0, 4],
+   [0, 0, 0, 6, 4, 0, 5, 0, 1],
+   [1, 3, 4, 0, 5, 0, 7, 0, 0],
+   [3, 6, 0, 0, 0, 4, 0, 0, 2],
+   [4, 0, 7, 2, 3, 0, 6, 0, 0],
+   [0, 0, 0, 7, 0, 0, 4, 5, 0]
+   ]}
 
-  it 'returns false if number is already in box' do
-    expect(box_ok?(4,5,test_board,8)).to eq false
+   it 'returns false if number is already in box' do
+    expect(box_ok?(3,4,test_board,6)).to eq false
   end
 
   it 'returns true if number is not already in box' do
-    expect(box_ok?(4,5,test_board,9)).to eq true
+    expect(box_ok?(4,5,test_board,3)).to eq true
   end
 
   it 'returns true if number is not already in box' do
